566ac5ebe49d67ad62b20dd03c5d1433516b5803
TheYibster <yiboliang02@gmail.com>
2021-09-06 21:19:22 -0700

    Now rolling 2 dice, added custom comments

--- dice_roller.py
+++ dice_roller.py
@@ -7,7 +7,19 @@ def main():
   for i in range(0, dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
-  print(f'you have rolled a total of {dice_sum}')
+    if roll == 1:
+      print(f'you rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print (f'you have rolled a {roll}! Congratulations!')
+    else:
+      print(f'You rolled a {roll}')
+
+  if dice_sum == 2:
+      print('snake eyes')
+  elif dice_sum == 12:
+      print(f'{dice_sum}! WOW!')
+  else:
+    print(f'you have rolled a total of {dice_sum}')
+
 if __name__== "__main__":
   main()
